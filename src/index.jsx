444c7ce552c02897d023a697eaaa7a69201caabf
Veronika Bendova <126337758+beveroni@users.noreply.github.com>
2023-05-17 17:11:42 +0200

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './style.css';
+
+const App = () => (
+  <>
+
+  </>
+);
+
+createRoot(
+  document.querySelector('#app'),
+).render(<App />);
